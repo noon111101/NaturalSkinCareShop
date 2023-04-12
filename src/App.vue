22b6108c18d1705e729ebeda82ffce6af934b89e
noon111101 <trieu11112001@gmail.com>
2023-04-12 14:14:10 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,70 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div id="app">
+    <Sidebar />
+    <div class="container-fluid">
+      <router-view />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
-
+<script>
+import Sidebar from '@/views/sidebar/SidebarVue.vue'
+import { sidebarWidth } from '@/views/sidebar/state'
+import { collapsed, toggleSidebar } from '@/views/sidebar/state'
+export default {
+  components:{Sidebar},
+  setup() {
+    return { sidebarWidth,collapsed, toggleSidebar }
+  },
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    },
+    showAdminBoard() {
+      if (this.currentUser && this.currentUser.roles) {
+        return this.currentUser.roles.includes('ROLE_ADMIN');
+      }
+      return false;
+    },
+    showModeratorBoard() {
+      if (this.currentUser && this.currentUser.roles) {
+        return this.currentUser.roles.includes('ROLE_MANAGE');
+      }
+      return false;
+    }
+  },
+  methods: {
+    logOut() {
+      this.$store.dispatch('auth/logout');
+      this.$router.push('/login');
+    }
+  }
+};
+</script>
 <style scoped>
-header {
-  line-height: 1.5;
+#app {
+  /*min-height: 100%;*/
+  /*width: 100%;*/
+  /*padding-top: 80px;*/
+  /*!*padding-bottom: 80px;*!*/
+  /*background-image: url("@/assets/background3.jpg") ;*/
+  /*background-position: center; !* Center the image *!*/
+  /*background-repeat: no-repeat; !* Do not repeat the image *!*/
+  /*background-size: cover; !**/
+  font-size: 16px;
+  font-family: 'Montserrat', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
